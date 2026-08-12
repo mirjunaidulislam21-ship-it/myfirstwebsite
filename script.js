64eb95fc040dac94471735e14ffef3be163a18b1
Junaid <mirjunaidulislam21@gmail.com>
2026-08-12 09:46:03 +0000

    upgrading my website

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function showMessage() {
-    alert("Hello Junaid! You are learning DevOps 🚀");
+    alert("Hello Junaid! 🚀 Keep learning AWS and DevOps!");
+}
+
+
+function toggleTheme() {
+
+    document.body.classList.toggle("dark");
+
+    const button =
+        document.querySelector(".theme-btn");
+
+    if (document.body.classList.contains("dark")) {
+
+        button.innerHTML = "☀️";
+
+    } else {
+
+        button.innerHTML = "🌙";
+
+    }
 }
